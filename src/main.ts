821ba7e950916a65a124e865465a861d4fddd217
John Mulhausen <jmulhausen@digitalocean.com>
2023-10-06 15:51:37 -0400

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -101,6 +101,7 @@ function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
 - Provide the response in following JSON format:  [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise return an empty array.
+- Only review changed markdown files and ignore changes to any other file type.
 - Write the comment in GitHub Markdown format.
 - IMPORTANT: NEVER suggest adding comments to the code.
 
