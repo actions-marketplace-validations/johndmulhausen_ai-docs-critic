4ab14fcaf084aed4109dc156383512bf12aa21f8
John Mulhausen <jmulhausen@digitalocean.com>
2023-10-06 15:54:28 -0400

    Update index.js

--- dist/index.js
+++ dist/index.js
@@ -119,12 +119,12 @@ function getBaseAndHeadShas(owner, repo, pull_number) {
     });
 }
 function createPrompt(file, chunk, prDetails) {
-    return `Your task is to review pull requests. Instructions:
+    return `Your task is to review pull requests that make changes to a technical documentation project to ensure they comply with the Vale rules checked into the repo and the Microsoft Style Guide. Instructions:
 - Provide the response in following JSON format:  [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise return an empty array.
+- Only review changed markdown files and ignore changes to any other file type.
 - Write the comment in GitHub Markdown format.
-- Use the given description only for the overall context and only comment the code.
 - IMPORTANT: NEVER suggest adding comments to the code.
 
 Review the following code diff in the file "${file.to}" and take the pull request title and description into account when writing the response.
